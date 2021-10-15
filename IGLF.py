1be5bc8358b24512437ace2a933de604dc7b65bb
Cory Holtshouser <coryholtshouser@gmail.com>
2021-10-15 14:44:26 -0400

    Delete IGLF.py

--- IGLF.py
+++
@@ -1,9 +0,0 @@
-def find_pressure(n,T,V,R=.082057):
-    pressure=(R)*n*T/V
-    return pressure
-
-test_n = 10
-test_T = 298
-test_V = 5
-test_R = 62.364 #Torr!
-print("Result:", find_pressure(test_n, test_T, test_V, R = test_R))
